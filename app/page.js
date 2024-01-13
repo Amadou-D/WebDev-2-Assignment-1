a189c469fbb417b38a0cfc9ccb2da6ebae50e709
Amadou <amadouamos@hotmail.com>
2024-01-13 12:46:30 -0700

    Small additions

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{' Amadou Diallo '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -108,6 +109,21 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+    <Link href="/app/newPage/page">
+    <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      New Page{' Test Page '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      Go to the new page you created.
+    </p>
+  </div>
+    </Link>
+
     </main>
   )
 }
